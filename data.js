a609ba281b02ff7722733526dd965272ae4cd94d
John Hill <jxh6494@rit.edu>
2018-07-22 12:45:58 -0400

    Copied all files

---
+++ data.js
@@ -0,0 +1,49 @@
+var data = new Object();
+
+data['init'] = ['images/nfl.jpg','Select A Conference', 'AFC','NFC'];
+data['AFC'] = ['images/afc_.png', 'Select a Divison', 'AFC North', 'AFC East', 'AFC South', 'AFC West'];
+data['NFC'] = ['images/nfc_.jpg','Select a Divison','NFC North', 'NFC East', 'NFC South', 'NFC West'];
+data['AFC North'] = ['images/afcnorth.jpg','Select a Team','Pittsburgh Steelers','Baltimore Ravens','Cincinnati Bengals','Cleveland Browns'];
+data['AFC East'] = ['images/afceast.jpg','Select a Team','New England Patriots','Buffalo Bills','Miami Dolphins','New York Jets'];
+data['AFC South'] = ['images/afcsouth.jpg','Select a Team','Jacksonville Jaguars','Tennessee Titans','Houston Texans','Indianapolis Colts'];
+data['AFC West'] = ['images/afcwest.jpg','Select a Team','KC Chiefs','LA Chargers','Oakland Raiders','Denver Broncos'];
+data['NFC North'] = ['images/nfcnorth.jpg','Select a Team','Minnesota Vikings','Detroit Lions','Green Bay Packers','Chicago Bears'];
+data['NFC East'] = ['images/nfceast.jpg','Select a Team','Philadelphia Eagles','Dallas Cowboys','Washington Redskins','New York Giants'];
+data['NFC South'] = ['images/nfcsouth.jpg','Select a Team','New Orleans Saints','Carolina Panthers','Atlanta Falcons','Tampa Bay Buccaneers'];
+data['NFC West'] = ['images/nfcwest.jpg','Select a Team','LA Rams','Seattle Seahawks','Arizona Cardinals','San Francisco 49ers'];
+
+
+var res = new Object();
+
+res['Cincinnati Bengals'] = ['images/bengals.jpg'];
+res['Baltimore Ravens'] = ['images/ravens.png'];
+res['Pittsburgh Steelers'] = ['images/steelers.jpg'];
+res['Cleveland Browns'] = ['images/browns.png'];
+res['New England Patriots'] = ['images/patriots.jpg'];
+res['Buffalo Bills'] = ['images/bills.jpg'];
+res['Miami Dolphins'] = ['images/dolphins.jpg'];
+res['New York Jets'] = ['images/jets.jpg'];
+res['Jacksonville Jaguars'] = ['images/jaguars.jpg'];
+res['Tennessee Titans'] = ['images/titans.jpg'];
+res['Houston Texans'] = ['images/texans.jpg'];
+res['Indianapolis Colts'] = ['images/colts.jpg'];
+res['KC Chiefs'] = ['images/chiefs.jpg'];
+res['LA Chargers'] = ['images/chargers.jpg'];
+res['Oakland Raiders'] = ['images/raiders.jpg'];
+res['Denver Broncos'] = ['images/broncos.jpg'];
+res['Minnesota Vikings'] = ['images/vikings.jpg'];
+res['Detroit Lions'] = ['images/lions.jpg'];
+res['Green Bay Packers'] = ['images/packers.jpg'];
+res['Chicago Bears'] = ['images/bears.jpg'];
+res['Philadelphia Eagles'] = ['images/eagles.jpg'];
+res['Dallas Cowboys'] = ['images/cowboys.jpg'];
+res['Washington Redskins'] = ['images/redskins.jpg'];
+res['New York Giants'] = ['images/giants.jpg'];
+res['New Orleans Saints'] = ['images/saints.jpg'];
+res['Carolina Panthers'] = ['images/panthers.jpg'];
+res['Atlanta Falcons'] = ['images/falcons.jpg'];
+res['Tampa Bay Buccaneers'] = ['images/bucs.jpg'];
+res['LA Rams'] = ['images/rams.jpg'];
+res['Seattle Seahawks'] = ['images/seahawks.jpg'];
+res['Arizona Cardinals'] = ['images/cardinals.jpg'];
+res['San Francisco 49ers'] = ['images/49ers.jpg'];
